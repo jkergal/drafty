c2390b35b59f0c10bcab0a8e2c2f8aa785d564c0
jkergal <hello@johannkergal.fr>
2023-02-20 21:33:26 +0100

    refactor: adds emojis IDs in env and constants files

--- src/constants/DISCORD_SERVER.ts
+++ src/constants/DISCORD_SERVER.ts
@@ -7,3 +7,12 @@ export const CHECKIN_CHANNEL_2 = process.env.CHECKIN_2_CHANNEL;
 export const CHECKIN_ASYNC_CHANNEL = process.env.CHECKIN_ASYNC_CHANNEL;
 export const TUTORIAL_CHANNEL = process.env.TUTORIAL_CHANNEL;
 export const GUILD_ID = process.env.GUILD_ID;
+
+export const MONDAY_EMOJI = process.env.MONDAY_EMOJI;
+export const TUESDAY_EMOJI = process.env.TUESDAY_EMOJI;
+export const WEDNESDAY_EMOJI = process.env.WEDNESDAY_EMOJI;
+export const THURSDAY_EMOJI = process.env.THURSDAY_EMOJI;
+export const FRIDAY_EMOJI = process.env.FRIDAY_EMOJI;
+export const SATURDAY_EMOJI = process.env.SAURDAY_EMOJI;
+export const SUNDAY_EMOJI = process.env.SUNDAY_EMOJI;
+export const ASYNC_EMOJI = ':alarm_clock:';
