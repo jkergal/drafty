a093f3fb5f1f1361e0a0ed1441d904bc861de7f5
jkergal <hello@johannkergal.fr>
2023-02-27 21:26:59 +0100

    test: adds test for formatScheduledMessage + refactors

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,35 @@
-import { ChatInputApplicationCommandData, Client, CommandInteraction } from 'discord.js';
+import {
+  ChatInputApplicationCommandData,
+  Client,
+  CommandInteraction,
+  Guild,
+  GuildEmoji,
+  TextChannel,
+} from 'discord.js';
+
+export interface DiscordApi {
+  getChannel: () => Channels;
+  getGuild: () => Guild | undefined;
+  getEmojis: () => {
+    monday: undefined;
+    tuesday: GuildEmoji | undefined;
+    wednesday: GuildEmoji | undefined;
+    thursday: GuildEmoji | undefined;
+    friday: GuildEmoji | undefined;
+    saturday: GuildEmoji | undefined;
+    sunday: GuildEmoji | undefined;
+    async: string;
+  };
+}
 
 export interface Command extends ChatInputApplicationCommandData {
   run: (client: Client, interaction: CommandInteraction) => void;
+}
+
+export interface Channels {
+  enrollmentsChannel: TextChannel | undefined;
+  checkinChannel1: TextChannel | undefined;
+  checkinChannel2: TextChannel | undefined;
+  checkinAsyncChannel: TextChannel | undefined;
+  tutorialChannel: TextChannel | undefined;
 }
